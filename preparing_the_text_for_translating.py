773731d838be0c70b54ee9390d736a479f1c37f0
Fatih <121514395+buyukakcali@users.noreply.github.com>
2023-12-16 00:42:27 +0100

    Update preparing_the_text_for_translating.py

--- preparing_the_text_for_translating.py
+++ preparing_the_text_for_translating.py
@@ -1,3 +1,35 @@
+"""
+Readme:
+ Kodun Amacı; çeviri yapılmak istenen metnin uzun olması ve genellikle bir pdf, word veya başka metin tabanlı bir dosyadan çeviri yapılmak istenmesi durumunda karşılaşılan zorlukları azaltmak, çevrilen metnin daha hızlı ve kolay biçimde anlaşılmasını sağlamaya yardımcı olmaktır. Çünkü başka formatlarda saklı tutulan bu metinlerde aslında olmaması gereken 'paragraf', 'tab' gibi çeviri sayfalarında çeviri kalitesini etkileyen unsurlar bulunmaktadır. Kod, farklı dosyalardan kopyaladığımız metni bu unsurlardan arındırmakta ve yazıyı nesir haline getirmektedir. Böylece daha doğru bir çeviri yapılmakta ve kullanıcısı için zaman tasarrufu sağlamaktadır.
+
+Açıklama:
+ - text_correction fonksiyonu dosya yolu parametresini isteyen, bir word dosyasından metinleri alıp iki aşamada string değişkene aktaran ve stringdeki 'paragraf', 'tab' kayıtlarını 'bir boşluk' karakteri ile değiştiren, ayrıca birden fazla boşluk olması halinde bunu tek boşluğa dönüştürerek bu stringi döndüren bir fonksiyondur.
+ - write2txt_file fonksiyonu dosya yolu ve text_correction fonksiyonundan elde ettiği stringi parametre olarak alıp ilgili yolda txt uzantılı bir metin belegesine bu stringi kaydetmektedir.
+ - write2docx_file fonksiyonu dosya yolu ve text_correction fonksiyonundan elde ettiği stringi parametre olarak alıp ilgili yolda docx uzantılı bir Word belegesine bu stringi kaydetmektedir.
+
+Parametreler:
+ - text_correction fonksiyonu için: file_path
+ -  write2txt_file fonksiyonu için: file_path ve text
+ -  write2docx_file fonksiyonu için: file_path ve text
+
+Kısıtlamalar:
+ - text_correction: yalnızca word dosyasını okuyabilir.
+ - Stringdeki paragrafları, girintileri ve metnin orijinal şeklini ortadan kaldırarak yazılanların kolaylıkla y-ayırt edilmesini zorlaştırır.
+ - Okunacak dosyanın tam yolunun koda elle girilmesi gerekir. Dosya yöneticisi ile seçmek mümkün değildir.
+ - Kodda hata kontrolü yapıları (try-catch) kullanılmamıştır. Kod kolaylıkla çökebilir. Mesela girdi dosya yolunda ilgili dosya bulunmazsa kod hata döndürür, ancak bu hatanın ne olduğu kod geliştiricisi tarafından açık bir şekilde belirtilmemiştir.
+
+Yapılar:
+ - for döngüsü çoğunlukla kullanılmıştır. 
+ - while döngüsü yalnızca bir yerde kullanılmıştır.
+ - docx modülü marifetiyle bir Document nesnesi oluşturularak dosya okunmuştur.
+ - with open yapısı dosya yazdırma işlemi için iki ayrı fonksiyonda kullanılmıştır.
+
+Çıktı:
+ -düzeltme işlemi tamamlandıktan sonra string metin dosyası ve word dosyasına yazılmıştır.
+ - Ekrana ıktıların yolu ile birlikte işlemin tamamlandığı bilgisi yazdırılmıştır.
+ """
+
+
 import docx
 
 
@@ -45,7 +77,7 @@ def write2txt_file(file_path, text):
 
 
 # Düzeltilmiş metni '.docx' uzantılı dosyaya yazdırıyoruz.
-def write2docx(file_path, text):
+def write2docx_file(file_path, text):
     document = docx.Document()
     document.add_paragraph(text)
     document.save(file_path)
@@ -56,8 +88,8 @@ def write2docx(file_path, text):
 
 if __name__ == '__main__':
     input_file = 'C:\\Users\\user\\Desktop\\giris.docx'
-    output_file_1 = 'C:\\Users\\user\\Desktop\\sonuc.txt'
-    output_file_2 = 'C:\\Users\\user\\Desktop\\docx_file.docx'
+    output_file_1 = 'C:\\Users\\user\\Desktop\\sonuc1.txt'
+    output_file_2 = 'C:\\Users\\user\\Desktop\\sonuc2.docx'
 
     print(write2txt_file(output_file_1, text_correction(input_file)))
-    print(write2docx(output_file_2, text_correction(input_file)))
+    print(write2docx_file(output_file_2, text_correction(input_file)))
